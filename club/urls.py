a0d8eaf82bd2cc0b368bf0a4290e9d842f061efc
Mateo <mateoeze2017@gmail.com>
2025-10-21 18:39:52 -0300

    Modifico color de boton filtrar que no se veía

--- club/urls.py
+++ club/urls.py
@@ -5,8 +5,13 @@ urlpatterns = [
     path("", index, name = "index"),
     path("jugadores", crearJugador, name = "jugadores_form"),
     path("jugadores_busqueda", listar_jugadores, name = "jugadores_list"),
+    path("jugadores/<int:dni>/eliminado", eliminar_jugador, name = "eliminar_jugador"),
+    
     path("patrocinadores_busqueda", listar_patrocinadores, name = "patrocinadores_list"),
-    path("mobiliario_busqueda", listar_mobiliario, name = "mobiliario_list"),
     path("patrocinadores", crearPatrocinador, name = "patrocinadores_form"),
+    #path("patrocinador_eliminado", eliminar_patrocinador, name = "eliminar_patrocinador"),
+    
+    path("mobiliario_busqueda", listar_mobiliario, name = "mobiliario_list"),
     path("mobiliario", crearMobiliario, name = "mobiliarios_form"),
+    #path("mobiliario_eliminado", eliminar_mobiliario, name = "eliminar_mobiliario"),
 ]
--- club/views.py
+++ club/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib import messages
 from club.forms import *
 from club.models import *
 
@@ -18,10 +19,13 @@ def crearJugador(request):
     if request.method == "POST":
         form = JugadoresForms(request.POST)
         if form.is_valid():
-            form.save()
+            jugador = form.save()
+            messages.success(request, f"El jugador con dni {jugador.dni} a sido creado correctamente")
             return redirect("jugadores_list")
     else:
         form = JugadoresForms()
+        
+    
     return render(request, "club/jugadores.html", {'form':form})
 
 def crearPatrocinador(request):
@@ -70,3 +74,22 @@ def listar_mobiliario(request):
         mobiliario = Mobiliario.objects.all().order_by("-nombre")
     
     return render(request, "club/mobiliario_list.html", {"mobiliario": mobiliario, "query":query})
+
+
+def eliminar_jugador(request, dni):
+    jugador = get_object_or_404(Jugadores, dni = dni) # Si existe más de uno o no existe, hay error
+    jugador.delete()
+    messages.success(request, f"El jugador con dni nro {dni} a sido eliminado correctamente")
+    return redirect('jugadores_list')
+    
+def modificar_jugador(request, dni):
+    jugador = get_object_or_404(Jugadores, dni = dni)
+    if request.method == 'POST':
+        form = JugadoresForms(request.POST, instance=jugador)
+        if form.is_valid():
+            form.save()
+            return redirect("jugadores_list")
+    else:
+        form = JugadoresForms(isinstance = jugador) # Puedo cambiar el formulario para restringir datos
+    return render(request, "coder/jugadores.html", {'form':form, 'edicion':True})
+
